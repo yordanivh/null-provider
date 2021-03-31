25e580fb6a1fc4b6ab3e8ce03e55191b3b9f8284
Yordan Halachev <yordan.i.halachev@gmail.com>
2021-03-31 10:21:16 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,24 @@
-provider "null" {}
+terraform {
+  required_version = "~> 0.14"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    tfe = {
+      source  = "hashicorp/tfe"
+      # Be sure this matches the version in the administration repo.
+      version = "~> 0.24.0"
+    }
+    local      = ">= 1.4"
+    null       = ">= 2.1"
+    template   = ">= 2.1"
+    random     = ">= 2.1"
+    kubernetes = ">= 1.11.1"
+  }
+}
+
 
 
 resource "null_resource" "example" {
